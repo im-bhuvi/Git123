ae0b072aadc4945acaaf2b69cb46d9b1bdb135fe
im-bhuvi <bhuvanesh.bhuvi1606@gamil.com>
2024-12-03 19:09:18 +0530

    git123

---
+++ src/org/git/Git.java
@@ -0,0 +1,22 @@
+package org.git;
+
+public class Git {
+
+	public static void main(String[] args) {
+		
+		int[]a= {1,2,3,4,5,10};
+		
+		System.out.println(a);
+		System.out.println(a.length);
+		System.out.println(a[0]);
+		
+		for(int i=0;i<a.length;i++) {
+			System.out.println(i);
+		}
+		
+		for(int x: a) {
+			System.out.println(x);
+		}
+	}
+	
+}
